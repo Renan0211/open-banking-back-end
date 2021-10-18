11572060f433016093ace73fd7ddd76f83a10ac4
Renan0211 <renan.antunes.rocha@gmail.com>
2021-10-18 18:55:11 -0300

    cria a migraçao e o model da tabela users

---
+++ src/models/user.js
@@ -0,0 +1,11 @@
+const User = (sequelize, DataTypes) => {
+  const User = sequelize.define("User", {
+    first_name: DataTypes.STRING,
+    last_name: DataTypes.STRING,
+    password: DataTypes.STRING,
+  });
+
+  return User;
+};
+
+module.exports = User;
