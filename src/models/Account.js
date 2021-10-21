1ce5385307fd274ead2bc25eadbea441a0dddb7e
Renan0211 <renan.antunes.rocha@gmail.com>
2021-10-20 21:40:48 -0300

    creates the Account model

---
+++ src/models/Account.js
@@ -0,0 +1,28 @@
+module.exports = (sequelize, DataTypes) => {
+  const Account = sequelize.define('Account',
+    {
+      id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+      balance: DataTypes.INTEGER,
+      user_id: DataTypes.INTEGER,
+      bank_id: DataTypes.INTEGER,
+    },
+    { timestamps: false },
+  );
+
+  Account.associate = (models) => {
+    models.Bank.belongsToMany(models.User, {
+      as: 'users',
+      through: Account,
+      foreignKey: 'bank_id',
+      otherKey: 'user_id',
+    });
+    models.User.belongsToMany(models.Book, {
+      as: 'banks',
+      through: Account,
+      foreignKey: 'user_id',
+      otherKey: 'bank_id',
+    });
+  };
+
+  return Account;
+};
