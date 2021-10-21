73302ba8a94a7f93b0d2a0f996dc0b5d80ce2f9d
Renan0211 <renan.antunes.rocha@gmail.com>
2021-10-20 21:59:53 -0300

    fixing an issue in the name of the properties in the transaction model

--- src/models/Transaction.js
+++ src/models/Transaction.js
@@ -2,7 +2,7 @@ module.exports = (sequelize, DataTypes) => {
   const Transaction = sequelize.define('Transaction', {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     sendingAccountId: { type: DataTypes.INTEGER, foreignKey: true },
-    receiving_account_id: { type: DataTypes.INTEGER, foreignKey: true },
+    receivingAccountId: { type: DataTypes.INTEGER, foreignKey: true },
     value: DataTypes.INTEGER,
     transactionType: DataTypes.SRING,
   },
