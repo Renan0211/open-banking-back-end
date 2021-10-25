6c81ce9c3f86683eaec7c7191b042610361a44b5
Renan0211 <renan.antunes.rocha@gmail.com>
2021-10-25 17:31:48 -0300

    enables the cors module so the front-end can make requests to this aplication

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyparser = require('body-parser');
+const cors = require('cors');
 
 const userController = require('./src/controllers/userController');
 const loginController = require('./src/controllers/loginController');
 
 const app = express();
+app.use(cors);
 app.use(bodyparser.json());
 app.use('/user', userController);
 app.use('/login', loginController);
