d47a10db68949cdc6273fdd29425d90d5209af4b
Renan0211 <renan.antunes.rocha@gmail.com>
2021-10-25 19:35:43 -0300

    changing the default port for the aplication to run and adding the .env into the .gitignore

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use('/login', loginController);
 
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`listening in port ${PORT}`));
 
 module.exports = app;
